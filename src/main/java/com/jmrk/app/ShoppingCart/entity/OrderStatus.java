e485925527d78144fea866ca2d92d3c8435f5012
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2025-01-23 14:18:38 -0400

    OrderStatus enum updated

--- src/main/java/com/jmrk/app/ShoppingCart/entity/OrderStatus.java
+++ src/main/java/com/jmrk/app/ShoppingCart/entity/OrderStatus.java
@@ -4,19 +4,19 @@ import lombok.Getter;
 
 @Getter
 public enum OrderStatus {
-    PENDING ("PENDING"),       // Order created but awaiting confirmation/payment
-    CONFIRMED("CONFIRMED"),     // Payment confirmed or order approved
-    PROCESSING("PROCESSING"),    // Order being prepared for shipment
-    SHIPPED("SHIPPED"),       // Order shipped to the customer
-    OUT_FOR_DELIVERY("OUT OF DELIVERY"), // Order is out for delivery
-    DELIVERED("DELIVERED"),     // Order delivered to the customer
-    COMPLETED("COMPLETED"),     // Fully resolved and no further actions
-    CANCELED("CANCELED"),      // Order canceled before fulfillment
-    RETURNED("RETURNED"),      // Order returned by the customer
-    REFUNDED("REFUNDED"),      // Payment refunded to the customer
-    FAILED("FAILED"),        // Order failed (e.g., payment or system issue)
-    ON_HOLD("ON HOLD"),       // Order paused for some reason
-    DISPUTED("DISPUTED")       // Order under dispute between buyer and seller
+    PENDING ("Pending"),       // Order created but awaiting confirmation/payment
+    CONFIRMED("Confirmed"),     // Payment confirmed or order approved
+    PROCESSING("Processing"),    // Order being prepared for shipment
+    SHIPPED("Shipped"),       // Order shipped to the customer
+    OUT_FOR_DELIVERY("Out of delivery"), // Order is out for delivery
+    DELIVERED("Delivered"),     // Order delivered to the customer
+    COMPLETED("Completed"),     // Fully resolved and no further actions
+    CANCELED("Canceled"),      // Order canceled before fulfillment
+    RETURNED("Returned"),      // Order returned by the customer
+    REFUNDED("Refunded"),      // Payment refunded to the customer
+    FAILED("Failed"),        // Order failed (e.g., payment or system issue)
+    ON_HOLD("On hold"),       // Order paused for some reason
+    DISPUTED("Disputed")       // Order under dispute between buyer and seller
 
     private final String displayName;
 
