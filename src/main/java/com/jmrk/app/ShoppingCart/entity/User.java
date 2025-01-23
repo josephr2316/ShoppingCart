d869293c82fc76e4a27d24b4eb221f8518b62eeb
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2025-01-22 22:04:48 -0400

    User entity created

---
+++ src/main/java/com/jmrk/app/ShoppingCart/entity/User.java
@@ -0,0 +1,44 @@
+package com.jmrk.app.ShoppingCart.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+
+import java.util.List;
+
+@Data
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false)
+    private int id;
+
+    @Column(nullable = false)
+    private String firstname;
+
+    @Column(nullable = false)
+    private String lastname;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ElementCollection(targetClass = String.class,fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role")
+    private List<String> roles;
+
+    @Column(nullable = false, columnDefinition = "boolean default true")
+    private boolean isActive;
+
+}
+
