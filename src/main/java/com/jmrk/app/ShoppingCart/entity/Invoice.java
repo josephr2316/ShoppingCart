4a82fbadc2d93659cd474afd99770c90a16c4aca
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2025-01-23 09:06:52 -0400

    Invoice entity created

--- src/main/java/com/jmrk/app/ShoppingCart/entity/Invoice.java
+++ src/main/java/com/jmrk/app/ShoppingCart/entity/Invoice.java
@@ -1,4 +1,34 @@
 package com.jmrk.app.ShoppingCart.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@Entity
 public class Invoice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @Column(nullable = false)
+    private String invoiceNumber;
+
+    @Column(nullable = false)
+    private LocalDate invoiceDate;
+
+    @Column(nullable = false)
+    private BigDecimal totalAmount;
+
+    @Column(nullable = false)
+    private String billingAddress;
+
 }
--- src/main/java/com/jmrk/app/ShoppingCart/entity/Shipping.java
+++ src/main/java/com/jmrk/app/ShoppingCart/entity/Shipping.java
@@ -16,12 +16,25 @@ public class Shipping {
     @JoinColumn(name = "order_id")
     private Order order;
 
+    @Column(nullable = false)
     private String address;
+
+    @Column(nullable = false)
     private String city;
+
+    @Column(nullable = false)
     private String state;
+
+    @Column(nullable = false)
     private String zipCode;
+
+    @Column(nullable = false)
     private String country;
+
+    @Column(nullable = false)
     private String shippingMethod; // Modify
+
+    @Column(nullable = false)
     private String shippingStatus; // Modify
 
 
