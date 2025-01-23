052b89faac6223909090816bf72af727d20786da
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2025-01-23 09:45:43 -0400

    Cart Entity updated

--- src/main/java/com/jmrk/app/ShoppingCart/entity/Cart.java
+++ src/main/java/com/jmrk/app/ShoppingCart/entity/Cart.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Data
 @Entity
@@ -14,9 +15,12 @@ public class Cart {
     @Column(updatable = false)
     private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false) // Verify
     private User user;
+
+     @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+     private List<CartItem> cartItems;
 
     @Column(nullable = false)
     private BigDecimal totalPrice;
